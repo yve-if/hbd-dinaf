3ab4a05826a0c60e228b1c1e8e1f110d373e90b3
yve-if <ayuu.new1@gmail.com>
2025-10-01 19:24:17 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -14,7 +14,7 @@ function validateForm() {
     Swal.fire({
       icon: "success",
       title: "Login berhasil!",
-      text: "Selamat datang ayanggg",
+      text: "Selamat datang dinnnn",
       showConfirmButton: false,
       timer: 1500,
     }).then(function () {
